2a11544294b32070639f63f4180c83cef666bb58
sohnryang <loop.infinitely@gmail.com>
2018-08-10 23:38:54 +0900

    added random_blocks example

---
+++ examples/random_blocks.py
@@ -0,0 +1,24 @@
+"""
+random_blocks.py -- add randomly placed blocks in the world.
+
+This script generates 1000 (default value) blocks in x, z range -50..50,
+and y range 0..50.
+
+"""
+
+
+import sys
+sys.path.append('..')
+
+from craft_api import CraftAPI
+import random
+
+api = CraftAPI()
+random.seed()
+
+BLOCK_COUNT = 1000    # number of blocks to create
+for _ in range(1000):
+    x = random.randint(-50, 50)
+    y = random.randint(0, 50)
+    z = random.randint(-50, 50)
+    api.add_brick_block((x, y, z))
